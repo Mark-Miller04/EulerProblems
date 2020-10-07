aecd22a10c22ea1472a80ec15087614965894050
Mark Miller <mark-miller04@outlook.com>
2020-10-07 10:56:49 -0400

    Add solution for problem 3.

--- EulerProject/Problems/Problem3.cs
+++ EulerProject/Problems/Problem3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using UtilityLibrary;
 
@@ -10,16 +11,32 @@ namespace EulerProject.Projects
 		{
 			_id = 3;
 			_tit = "Largest Prime Factor";
-			_que = "What is the largest prime factor of the number 600851475143?";
+			_que = "What is the largest prime factor of the number 600,851,475,143?";
 			_eff = BigO.On;
 		}
 
 		public override void Solve()
 		{
-			int[] primes = MathUtils.PrimeGeneratorLight(100);
-			//BigInteger[] primes = MathUtils.PrimeGeneratorHeavy(100);
-			Console.WriteLine(primes.ToString());
-			Console.WriteLine($"The largest prime factor is {primes[primes.Length - 1]}.");
+			int limit = (int)(Math.Sqrt(600851475143)) + 1;
+			int[] primes = MathUtils.PrimeGeneratorLight(limit);
+
+			List<int> factors = new List<int>();
+			int high = 0;
+			foreach(int i in primes) {
+				if (600851475143 % i == 0) {
+					factors.Add(i);
+					high = i;
+				}
+			}
+
+			string str = "";
+			foreach(int i in factors) {
+				str += $"{i}, ";
+			}
+			
+			Console.WriteLine(str);
+			Console.WriteLine("Above is the list of all prime factors for the given input.");
+			Console.WriteLine($"Therefore, the largest prime factor is {high}.");
 		}
 	}
 }
